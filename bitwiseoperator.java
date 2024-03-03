261fac932711cb4578c2e9cf55cde2e05c6bcb32
ragini-6911 <161419235+ragini-6911@users.noreply.github.com>
2024-03-03 12:20:00 +0530

    Add files via upload

---
+++ bitwiseoperator.java
@@ -0,0 +1,16 @@
+package com.javaapplication;
+
+public class bitwiseoperator {
+
+	public static void main(String[] args) {
+		int x= 10;
+		int y= 5;
+	int	oroperator= x|y;
+	int andoperator= x&y;
+	System.out.println(oroperator);
+	System.out.println(andoperator);
+		
+
+	}
+
+}
---
+++ ternaryoperator.java
@@ -0,0 +1,5 @@
+package com.javaapplication;
+
+public class ternaryoperator {
+
+}
