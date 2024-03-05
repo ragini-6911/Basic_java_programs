c718484cf20cdfc295c25ab6238e7d74e4cd235b
ragini-6911 <161419235+ragini-6911@users.noreply.github.com>
2024-03-05 11:32:07 +0530

    Add files via upload

---
+++ arrays_binarysearch.java
@@ -0,0 +1,22 @@
+package com.javaapplication;
+import java.util.Arrays;
+
+
+public class arrays_binarysearch {
+	
+	
+
+		public static void main(String[] args) {
+			int arr[]= {10,3,2,3,9,5};
+			
+				Arrays.binarySearch(arr,9);
+				
+			
+			System.out.println( "9  is  found at the index " + Arrays.binarySearch(arr,9));
+			
+
+		}
+
+	}
+
+
