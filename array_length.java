401c3428dec55ca89b7eda6b1f5c150f35c60bc6
ragini-6911 <161419235+ragini-6911@users.noreply.github.com>
2024-03-05 11:42:02 +0530

    Add files via upload

---
+++ array_length.java
@@ -0,0 +1,15 @@
+package com.javaapplication;
+
+public class array_length {
+
+	public static void main(String[] args) {
+		int array[]= {1,4,55,89,100};
+int len=0;
+		for(int i=0;i<array.length;i++) {
+			 
+			 len++;
+		}
+System.out.println("length of the array is "+ len);
+	}
+
+}
