9b0efdedf737747f0aeb160ac14d3f9c9ba90757
ragini-6911 <161419235+ragini-6911@users.noreply.github.com>
2024-03-05 11:44:09 +0530

    Add files via upload

---
+++ array_sum.java
@@ -0,0 +1,18 @@
+package com.javaapplication;
+
+public class array_sum {
+
+	public static void main(String[] args) {
+		int array[]= {1,4,55,89,100};
+int sum=0;
+		for(int i=0;i<array.length;i++) {
+			sum=sum+array[i];
+			 
+			 sum++;
+		}
+System.out.println("sum of the array is "+ sum);
+	}
+
+}
+
+
